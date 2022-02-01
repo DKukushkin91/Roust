35d1e839410be6a5fdb3825dadc90b86681bd313
Aleksandr Lebedev <aleksandr.lebedev@promo.ru>
2022-02-01 18:04:49 +0300

    contact

--- htdocs/local/templates/roust/components/bitrix/news.list/contact_factories/template.php
+++ htdocs/local/templates/roust/components/bitrix/news.list/contact_factories/template.php
@@ -4,8 +4,8 @@
 	<?foreach ($arResult["ITEMS"] as $key => $value) {?>
 	<!-- <pre><?print_r($value['PROPERTIES']['FACTORY']['VALUE']);?></pre> -->
 	<li class="contacts__li">
-		<?if ($value['PROPERTIES']['ADRESS']['VALUE'] != $oldcountru && !empty($value['PROPERTIES']['ADRESS']['VALUE'])){?>
-			<h2 class="contacts__title"><?=$value['PROPERTIES']['ADRESS']['VALUE']?></h2>
+		<?if ($value['PROPERTIES']['COUNTRY']['VALUE'] != $oldcountru && !empty($value['PROPERTIES']['COUNTRY']['VALUE'])){?>
+			<h2 class="contacts__title"><?=$value['PROPERTIES']['COUNTRY']['VALUE']?></h2>
 		<?}?>
 		<div class="contacts__li-content">
 			<h3 class="contacts__li-title<?if($value['PROPERTIES']['FACTORY']['VALUE']=="Y"){echo ' contacts__li-title--factory';}?>"><?=$value["NAME"]?></h3>
@@ -21,7 +21,7 @@
 		</div>
 	</li>
 	<?
-	$oldcountru = $value['PROPERTIES']['ADRESS']['VALUE'];
+	$oldcountru = $value['PROPERTIES']['COUNTRY']['VALUE'];
 	}
 	?>
 </ul>
