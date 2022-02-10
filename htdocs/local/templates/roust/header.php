6ad1bf4efa693887211accdd0a4b89eb9bccfd5d
Aleksandr Lebedev <aleksandr.lebedev@promo.ru>
2022-02-10 13:41:18 +0300

    test

--- htdocs/local/templates/roust/header.php
+++ htdocs/local/templates/roust/header.php
@@ -3,11 +3,12 @@ use Bitrix\Main\Page\Asset;
 use Bitrix\Main\Application;
 global $APPLICATION;
 global $elementId;
+global $_REQUEST;
 ?>
 <?
-if(!empty($elementId)){
+if(!empty($_REQUEST['PRODUCT_CODE'])){
 	$page_name ='brands-item';
-	$DataThisProduct = Products::getDataProductByCode($elementId);
+	$DataThisProduct = Products::getDataProductByCode($_REQUEST['PRODUCT_CODE']);
 	$DataThisProduct_img = CFile::ResizeImageGet($DataThisProduct["BACKGROUND"], array(), BX_RESIZE_IMAGE_PROPORTIONAL_ALT, false);
 }else{
 	switch ($APPLICATION->GetCurPage(false)) {
@@ -133,79 +134,24 @@ if(!empty($elementId)){
 					</div>
 				</header>
 				<?
-				if(!empty($elementId)){?>
-				<section class="top brands-item__top-section">
-					<div class="container brands-item__top-container js-brands-item-container">
-						<div class="top__wrap top__wrap--brands-item">
-							<nav class="breadcrumbs js-opacity brands-item__breadcrumbs">
-								<? 
-								$APPLICATION->IncludeComponent("bitrix:breadcrumb","",Array(
-									"START_FROM" => "1",
-									"PATH" => "",
-									"SITE_ID" => "s1"
-									)
-								); ?>
-							</nav>
-							<div class="top__content top__content--brands-item js-opacity">
-								<h1 class="top__title top__title--brands-item"><?$DataThisProduct["NAME"];?></h1>
-								<p class="top__text top__text--brands-item"><?$DataThisProduct["DESCRPTION"];?></p>
-								<a class="top__link" href="<?$DataThisProduct["LINK"];?>">
-									<span class="top__link-text">Visit brand site</span>
-								</a>
-							</div>
-							<ul class="top__list-awards js-opacity">
-								<li class="top__item-awards">
-									<picture>
-										<source
-											srcset="/f/img/responsive/m-Awards-rs1@1x.webp 1x, /f/img/responsive/m-Awards-rs1@2x.webp 2x"
-											media="(max-width: 639px)" type="image/webp" />
-										<source srcset="/f/img/responsive/m-Awards-rs1@1x.png 1x, /f/img/responsive/Awards-rs1@2x.png 2x"
-											media="(max-width: 639px)" type="image/png" />
-										<source srcset="/f/img/responsive/Awards-rs1@1x.webp 1x, /f/img/responsive/Awards-rs1@2x.webp 2x"
-											type="image/webp" />
-										<source srcset="/f/img/responsive/Awards-rs1@1x.png 1x, /f/img/responsive/Awards-rs1@2x.png 2x"
-											type="image/png" /><img src="/f/img/responsive/Awards-rs1@1x.png" alt="" />
-									</picture>
-								</li>
-								<li class="top__item-awards">
-									<picture>
-										<source
-											srcset="/f/img/responsive/m-awards-rs2@1x.webp 1x, /f/img/responsive/m-awards-rs2@2x.webp 2x"
-											media="(max-width: 639px)" type="image/webp" />
-										<source srcset="/f/img/responsive/m-awards-rs2@1x.png 1x, /f/img/responsive/awards-rs2@2x.png 2x"
-											media="(max-width: 639px)" type="image/png" />
-										<source srcset="/f/img/responsive/awards-rs2@1x.webp 1x, /f/img/responsive/awards-rs2@2x.webp 2x"
-											type="image/webp" />
-										<source srcset="/f/img/responsive/awards-rs2@1x.png 1x, /f/img/responsive/awards-rs2@2x.png 2x"
-											type="image/png" /><img src="/f/img/responsive/awards-rs2@1x.png" alt="" />
-									</picture>
-								</li>
-							</ul>
-							<div class="top__img-item js-anim-size">
-								<div class="top__img-wrapper">
-									<div class="top__picture">
-										<picture>
-											<source
-												srcset="/f/img/responsive/m-rs-bottle@1x.webp 1x, /f/img/responsive/m-rs-bottle@2x.webp 2x"
-												media="(max-width: 639px)" type="image/webp" />
-											<source srcset="/f/img/responsive/m-rs-bottle@1x.png 1x, /f/img/responsive/rs-bottle@2x.png 2x"
-												media="(max-width: 639px)" type="image/png" />
-											<source srcset="/f/img/responsive/rs-bottle@1x.webp 1x, /f/img/responsive/rs-bottle@2x.webp 2x"
-												type="image/webp" />
-											<source srcset="/f/img/responsive/rs-bottle@1x.png 1x, /f/img/responsive/rs-bottle@2x.png 2x"
-												type="image/png" /><img src="/f/img/responsive/rs-bottle@1x.png" alt="" />
-										</picture>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<button class="top__scroll-btn js-scroll-btn">
-						<svg class="top__scroll-icon" width="144" height="50">
-							<use href="/f/img/sprite.svg#flag"></use>
-						</svg>
-					</button>
-				</section>
+				if(!empty($_REQUEST['PRODUCT_CODE'])){?>
+
+
+<?
+				// Хедер для продукта
+				$APPLICATION->IncludeComponent(
+					'bitrix:news.detail',
+					'brands_header',
+					array(
+						'IBLOCK_ID' => CIBlockTools::GetIBlockId('brands'),
+						'ELEMENT_CODE' => $_REQUEST['PRODUCT_CODE'],
+						'CACHE_TYPE' => 'Y',
+						'CACHE_TIME' => 604800,
+						'FIELD_CODE' => array('*'),
+						'PROPERTY_CODE' => array('*')
+					)
+				);?>
+
 				<?}
 				elseif($page_name=="main-page"){
 				}
@@ -228,6 +174,6 @@ if(!empty($elementId)){
 
 
 
-
 			</div>
 			<main class="main main--<?=$page_name?>">
+<!-- <pre><?print_r($_REQUEST)?></pre> -->
