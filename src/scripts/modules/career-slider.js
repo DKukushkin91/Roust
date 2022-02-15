40b2efeb7e9061e1dbde99f06e7ff34fc95daebe
Dmitrij <dmitrij.kukushkin@promo.ru>
2022-02-15 14:12:08 +0500

    add adaptive career

--- src/scripts/modules/career-slider.js
+++ src/scripts/modules/career-slider.js
@@ -3,12 +3,15 @@ export const getCareerSlider = () => {
 		const slider = document.querySelector('.js-career-slider');
 
 		new Swiper(slider, {
-			slidesPerView: 'auto',
+			slidesPerView: 1,
+			pagination: {
+				el: ".js-office-pagination",
+			},
 			spaceBetween: 35,
 			navigation: {
 				nextEl: '.js-career-btn-next',
 				prevEl: '.js-career-btn-prev'
-			}
+			},
 		})
 	}
 }
